fd88d99d9d1456a08f1ee12c7d94c4a412448f9e
CrazyFraug <bozec.arnaud@free.fr>
2020-12-10 20:35:01 +0100

    le traitement du message est separe de la lecture buffer

--- Arduino-Machine-A-Etats.ino
+++ Arduino-Machine-A-Etats.ino
@@ -14,12 +14,14 @@
 #include "Arduino.h"
 
 //void serialEvent();
-//void readSerial();
+void readSerialWithStateMachine(String& amsgModbus);
+void readMsgModbus(String& amsgModbus);
 void gereLED();
 
 const int maxBufferModbus = 200;    // max size for modbus buffer
 String inputString = "";     // a String to hold incoming data
 //bool isStringComplete = false; // whether the string is complete
+String msgModbus = "";     // a String to hold a complete modbus message
 
 // we implement a state machine, we declare all possible sate values
 enum class StateM { idle, received, rx_waiting_end_lf, emission, emission_start, emission_end };
@@ -28,7 +30,7 @@ StateM stateOfMachine = StateM::idle;
 // the programme will be split in functions, each correspond to a state value
 void stateIdle();
 void stateReceived();
-void stateRXWaitingEndLF();
+void stateRXWaitingEndLF(String& amsgModbus);
 void stateEmission();
 void stateEmissionStart();
 void stateEmissionEnd();
@@ -50,19 +52,11 @@ void setup()
 
 void loop()
 {
-    // le comportement depend de l etat de la machine
-    if (stateOfMachine == StateM::idle)   stateIdle();
+    // lit les donnees venant du serial et met a jour msgModbus si il vient de recevoir un message
+    readSerialWithStateMachine(msgModbus);
 
-    else if (stateOfMachine == StateM::received)   stateReceived();
-
-    else if (stateOfMachine == StateM::rx_waiting_end_lf)    stateRXWaitingEndLF();
-
-    else   {
-        Serial.println(String("state of machine inconnu : "+
-                       String(static_cast<int>(stateOfMachine)) +
-                       ", retour a l etat idle"));
-        stateOfMachine = StateM::idle;
-    }
+    // traite un message modbus
+    readMsgModbus(msgModbus);
 
     // autres actions, en parallele de l etat  stateOfMachine
     gereLED();
@@ -73,6 +67,48 @@ void loop()
 void gereLED()
 {
     // pas code
+}
+
+void readSerialWithStateMachine(String& amsgModbus)
+{
+    // le comportement depend de l etat de la machine
+    if (stateOfMachine == StateM::idle)   stateIdle();
+
+    else if (stateOfMachine == StateM::received)   stateReceived();
+
+    else if (stateOfMachine == StateM::rx_waiting_end_lf)    stateRXWaitingEndLF(amsgModbus);
+
+    else   {
+        Serial.println(String("state of machine inconnu : "+
+                       String(static_cast<int>(stateOfMachine)) +
+                       ", retour a l etat idle"));
+        stateOfMachine = StateM::idle;
+    }
+
+}
+
+void readMsgModbus(String& amsgModbus)
+{
+    // Si le message est vide, on peut s en aller de suite
+    if (amsgModbus.length() == 0)   return;
+
+    // on affiche le message
+    Serial.println(); // faire un saut de ligne entre les réponses
+    Serial.print("Le message recu est : ");
+    Serial.println(amsgModbus);
+
+    // test de la réponse du joueur
+    if (amsgModbus == "LED2ON\r\n")
+    {
+        Serial.println("Vous voulez allumer la LED 2");
+    }
+    else
+    {
+        Serial.println("Erreur, commande invalide...");
+    }
+
+    // comme le message a ete traite, je l efface
+    amsgModbus = "";
 }
 
 void stateIdle()
@@ -136,7 +172,7 @@ void stateReceived()
     }
 }
 
-void stateRXWaitingEndLF()
+void stateRXWaitingEndLF(String& amsgModbus)
 {
     while (Serial.available() > 0)
     {
@@ -164,21 +200,9 @@ void stateRXWaitingEndLF()
             return;     //   <----   sortie de fonction   ---->
         }
 
-        // ici on considere que la chaine est complete et on la traite
 
-        Serial.println(); // faire un saut de ligne entre les réponses
-        Serial.print("Le message recu est : ");
-        Serial.println(inputString);
-
-        // test de la réponse du joueur
-        if (inputString == "LED2ON\r\n")
-        {
-            Serial.println("Vous voulez allumer la LED 2");
-        }
-        else
-        {
-            Serial.println("Erreur, commande invalide...");
-        }
+        // ici on considere que la chaine est complete et on la transmet
+        amsgModbus = inputString;
 
         // clear the string
         inputString = "";
